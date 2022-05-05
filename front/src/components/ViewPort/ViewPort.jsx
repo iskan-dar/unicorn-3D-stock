a6470118d566ea93d70865ea13273eec93f4c052
“AntonAtnagulov” <bebebe59@gmail.com>
2022-05-05 21:57:31 +0300

    ViewPort1

--- front/src/components/ViewPort/ViewPort.jsx
+++ front/src/components/ViewPort/ViewPort.jsx
@@ -8,6 +8,7 @@ const ViewPort = () => {
 
   useEffect(() => {
     const scene = new THREE.Scene();
+    scene.background = new THREE.Color( "#ffffff" )
     const camera = new THREE.PerspectiveCamera( 75, 500/500, 0.1, 1000 );
     const renderer = new THREE.WebGLRenderer();
 
@@ -17,7 +18,7 @@ const ViewPort = () => {
     const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 
     const light = new THREE.SpotLight()
-        light.position.set(10, 10, 60)
+        light.position.set(10, 10, 50)
         scene.add(light)
 
     const material = new THREE.MeshPhongMaterial({
@@ -25,33 +26,58 @@ const ViewPort = () => {
         flatShading: true,
       });
 
-    const cube = new THREE.Mesh( geometry, material );
+    // const cube = new THREE.Mesh( geometry, material );
     
     const loader = new STLLoader()
         loader.load(
-        '/Skeleton5.stl',
+        '/Sentinel.stl',
         function (geometry) {
             const mesh = new THREE.Mesh(geometry, material)
-            // mesh.position(0,0,0)
+            // const position = new THREE.Vector3();
+            // position.x = 1
+            // position.y = 1
+            // position.z = 1
+            // console.log("=====>",position)
+            // console.log(mesh.computeBoundingBox())
+            // mesh.position.set(0,0,0)
+            
             scene.add(mesh)
+            mesh.updateMatrixWorld(true);
+
+            mesh.geometry.computeBoundingBox();
+
+            var boundingBox = mesh.geometry.boundingBox;
+            
+            var position = new THREE.Vector3();
+            position.subVectors( boundingBox.max, boundingBox.min );
+            position.multiplyScalar( 0.5 );
+            position.add( boundingBox.min );
+            
+            position.applyMatrix4( mesh.matrixWorld );
+            
+            console.log(position.x + ',' + position.y + ',' + position.z);
+
+            mesh.position.set(-position.x, -position.y, -position.y)
+
+            // camera.position.x(position.x)
+
+            // camera.lookAt(position);
+
+
             const animate = function () {
                 requestAnimationFrame( animate );
               //   cube.rotation.x += 0.01;
               mesh.rotation.y += 0.01;
                 renderer.render( scene, camera );
               }
-              animate();
-        },
-        (xhr) => {
-            console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
-        },
-        (error) => {
-            console.log(error)
+
+            animate();
         }
     )
 
-    camera.position.z = 70;
-    camera.lookAt(0, 0, 0);
+    
+    camera.position.z = 150;
+    // camera.lookAt(SCENE.position);
 
     return () => mountRef.current.removeChild( renderer.domElement);
   }, []);
